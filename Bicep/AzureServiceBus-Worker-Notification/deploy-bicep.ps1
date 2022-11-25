d522bed9ea3c55b9ccb628c44f885cecdc120c3c
Joao Goncalves <jgoncalves@microsoft.com>
2022-11-25 18:57:11 +0000

    added k8s commands

--- Bicep/AzureServiceBus-Worker-Notification/deploy-bicep.ps1
+++ Bicep/AzureServiceBus-Worker-Notification/deploy-bicep.ps1
@@ -2,7 +2,7 @@ $ManagementSubscriptionId="CHANGE_ME"
 $BicepFile = "main.bicep"
 $ParamsFile = "main.parameters.json"
 $Location = "northeurope"
-$LZ1RG = "JPRG-ALZ-LZ1"
+$LZ1RG = "JPRG-ALZ-LZ-1"
 
 az account set --subscription $ManagementSubscriptionId
 
@@ -21,3 +21,16 @@ az aks update --resource-group $LZ1RG --name $AKS --enable-keda
 
 # Get AKS Credentials
 az aks get-credentials --resource-group $LZ1RG --name $AKS
+
+# --- K8s Operations ---
+
+# Create K8s Namespace
+$K8sNamespace = "keda-dotnet-sample"
+kubectl create namespace $K8sNamespace
+
+# Create KEDA Secret
+$QueueName = "input-queue"
+$SBUS=$(az servicebus namespace list -g $LZ1RG --query "[0].name" -o tsv)
+
+$KEDACONNSTRING=$(az servicebus queue authorization-rule keys list -g $LZ1RG --namespace-name $SBUS --queue-name $QueueName -n keda-monitor --query primaryConnectionString -o tsv)
+kubectl create secret -n $K8sNamespace generic keda-monitor-secret --from-literal servicebus-connectionstring="$KEDACONNSTRING"
